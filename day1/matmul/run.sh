1220e3831f9f1057770a6cf5d974ce0fcbd33fb4
Litvinov Sergey <slitvinov@gmail.com>
2012-01-16 17:30:36 +0300

    Add several runs

--- day1/matmul/run.sh
+++ day1/matmul/run.sh
@@ -2,10 +2,16 @@
 
 export COMPUTE_PROFILE=1
 export COMPUTE_PROFILE_CONFIG=cuda_profile.cfg
+export COMPUTE_PROFILE_CSV=1
 
+nlinst="16 32 64 128 256 512 1024 2048"
+for n in ${nlinst}; do
+    export COMPUTE_PROFILE_LOG=profile.matmul1.n${n}.log 
+    ./matmul1 ${n}
+done
 
-export COMPUTE_PROFILE_LOG=profile.matmul1.log 
-./matmul1 2048
+for n in ${nlinst}; do
+    export COMPUTE_PROFILE_LOG=profile.matmul2.n${n}.log 
+    ./matmul2 ${n}
+done
 
-export COMPUTE_PROFILE_LOG=profile.matmul2.log 
-./matmul2 2048
