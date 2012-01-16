5633d18cd0a0e8a574770528e23f36f2870f39e8
Litvinov Sergey <slitvinov@gmail.com>
2012-01-16 17:14:26 +0300

    Copy matmul2 from the solution

--- day1/matmul/matmul2.cu
+++ day1/matmul/matmul2.cu
@@ -4,22 +4,48 @@
 
 __global__ void matmul2(float* a, float* b, int n, float* c)
 {
-    // Base indexes inside A and B, index in C
-    // ...
+    // Base indexes inside A and B
+    int ia = (blockDim.y * blockIdx.y) * n;
+    int ib = blockDim.x * blockIdx.x;
+    
+    // Subindex inside a "tile"
+    int tileidx = n * threadIdx.y + threadIdx.x;
+    
+    // Index in C
+    int ic = ia + ib + tileidx;
+
+    float sum = 0.0f;
+    int aoff = 0, boff = 0;
 
     // Shared memory for the "tile" sub-matrix of A and B
-    // ...
+    __shared__ float as [BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float bs [BLOCK_SIZE][BLOCK_SIZE];
 
     // Go through "tiles" of size blockDim.x * blockDim.y
-    // For each tile: load to shared memory, multiply two
-    // tiles using shared memory
-    // ... 
+    for (; aoff < n; aoff += blockDim.x, boff += blockDim.y * n)
+    {
+       // Load the "tile" matrices from global memory to shared memory
+        as [threadIdx.y][threadIdx.x] = a [ia + aoff + tileidx];
+        bs [threadIdx.y][threadIdx.x] = b [ib + boff + tileidx];
 
-    // Write the resulting sum to global memory
-    // ...
+        // Synchronize to make sure the matrices are loaded
+        __syncthreads();
+
+        // Multiply the two matrices
+        for (int k = 0; k < BLOCK_SIZE; k++)
+            sum += as [threadIdx.y][k] * bs [k][threadIdx.x];
+
+        // Synchronize to make sure that the preceding
+        // computation is done before loading two new
+        // sub-matrices of A and B in the next iteration
+        __syncthreads();
+    }
+
+    // Write the block sub-matrix to global memory
+    // each thread writes one element
+    c [ic] = sum;
 }
 
 
 #define kernel matmul2
 #include "main.h"
-
