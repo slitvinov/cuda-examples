655d769d12becaa29633cf3635f65c1408aa6ac2
Litvinov Sergey <slitvinov@gmail.com>
2012-01-17 14:56:36 +0300

    sorting by key works

--- day2/thrust/sort_by_key.cu
+++ day2/thrust/sort_by_key.cu
@@ -4,12 +4,26 @@
 #include <thrust/copy.h>
 #include <cstdio>
 #include <cstdlib>
+#include <thrust/random.h>
 
 void usage(const char* filename)
 {
 	printf("Sort the random key-value data set of the given length by key.\n");
 	printf("Usage: %s <n>\n", filename);
 }
+
+thrust::host_vector<int> random_vector(size_t N)
+{
+    thrust::host_vector<int> vec(N);
+    static thrust::default_random_engine rng;
+    static thrust::uniform_int_distribution<int> dist(0, 9);
+
+    for (size_t i = 0; i < N; i++)
+        vec[i] = dist(rng);
+
+    return vec;
+}
+
 
 using namespace thrust;
 
@@ -37,13 +51,8 @@ int main(int argc, char* argv[])
 	// TODO: Generate random keys and values on host
 	// host_vector<int> ...
 	// generate( ...
-	thrust::host_vector<int> h_keys(n);
-	thrust::host_vector<int> h_keys(n);
-
-	thrust::uniform_real_distribution<float> u01(0.0f, 1.0f);
-	for (size_t i=0; i<n; i++) {
-	  
-	}
+	thrust::host_vector<int> h_keys = random_vector(n);
+	thrust::host_vector<int> h_vals = random_vector(n);
 
 	// Print out the input data if n is small.
 	if (n <= printable_n)
@@ -56,13 +65,20 @@ int main(int argc, char* argv[])
 
 	// TODO: Transfer data to the device.
 	// device_vector<int> ...
+	device_vector<int> keys = h_keys;
+	device_vector<int> values = h_vals;
 
 	// TODO: Use sort_by_key or stable_sort_by_key to sort
 	// pairs by key.
 	// sort_by_key( ...
+	thrust::sort_by_key(keys.begin(), keys.end(), 
+			    values.begin(), thrust::greater<int>());
+
 
 	// TODO: Transfer data back to host.
-	// copy( ...
+	thrust::copy(keys.begin(), keys.end(), h_keys.begin());
+	thrust::copy(values.begin(), values.end(), h_vals.begin());
+	
 
 	// Print out the output data if n is small.
 	if (n <= printable_n)
